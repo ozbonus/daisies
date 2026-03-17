f4a4ccca404a87c5250ff81c4959c02b77699b85
Christopher Perry <ozbonus@gmail.com>
2026-03-17 15:20:14 +0800

    Add basic argument parsing.

--- main.py
+++ main.py
@@ -1,7 +1,41 @@
+from dataclasses import dataclass
 import os
+import argparse
+from pathlib import Path
+
 from dotenv import load_dotenv
 from elevenlabs import ElevenLabs
 from elevenlabs_client import ElevenLabsClient
+
+
+def parse_args() -> list[Path]:
+    parser = argparse.ArgumentParser(
+        prog="daisies",
+        description="A utility for generating audio and timestamps from text dialog scripts.",
+        epilog="Examples:\n  daisies ./scripts\n  daisies ./scripts/dialog.json",
+    )
+
+    parser.add_argument(
+        "input", type=Path, help="A JSON script or directory that contains JSON scripts"
+    )
+
+    args = parser.parse_args()
+    path: Path = args.input
+
+    if not path.exists():
+        parser.error(f"Not found: {path}")
+
+    if path.is_file():
+        if path.suffix.lower() != ".json":
+            parser.error(f"Expected JSON file, but got: {path}")
+        return [path]
+    elif path.is_dir():
+        scripts = list({*path.glob("*.json"), *path.glob("*.JSON")})
+        if not scripts:
+            parser.error(f"No JSON files found in directory: {path}")
+        return scripts
+    else:
+        parser.error(f"Input must be a file or directory, not: {path}")
 
 
 def write_audio():
@@ -13,6 +47,7 @@ def write_segments():
 
 
 def main():
+    parse_args()
     load_dotenv()
     api = ElevenLabs(
         base_url="https://api.elevenlabs.io",
