f62c3841eb9bdd2af8cd90505c90a1bac042b284
Christopher Perry <ozbonus@gmail.com>
2026-03-31 15:10:01 +0800

    Add a schema for output scripts.

--- json_schema.py
+++ json_schema.py
@@ -28,3 +28,35 @@ INPUT = {
     "required": ["locale", "lines"],
     "additionalProperties": False,
 }
+
+OUTPUT = {
+    "type": "object",
+    "properties": {
+        "locale": {
+            "type": "object",
+            "properties": {
+                "languageCode": {"type": "string"},
+                "countryCode": {"type": "string"},
+            },
+            "required": ["languageCode"],
+            "additionalProperties": False,
+        },
+        "lines": {
+            "type": "array",
+            "minItems": 1,
+            "items": {
+                "type": "object",
+                "properties": {
+                    "startTime": {"type": "integer"},
+                    "endTime": {"type": "integer"},
+                    "speaker": {"type": ["string", "null"]},
+                    "text": {"type": "string"},
+                },
+                "required": ["speaker", "text"],
+                "additionalProperties": False,
+            },
+        },
+    },
+    "required": ["locale", "lines"],
+    "additionalProperties": False,
+}
