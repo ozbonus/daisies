56e05bf1fe9f0604b17ee8a814f77867b5a9be49
Christopher Perry <ozbonus@gmail.com>
2026-04-07 14:59:16 +0800

    Generate a script that follows ouptu schema.

--- output_writer.py
+++ output_writer.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from typing import NamedTuple
 
 from dialog_script import DialogScript
 from elevenlabs_client import DialogResponse
@@ -27,14 +28,34 @@ class OutputWriter:
         self.audio_write_path = write_dir / f"{input_script.stem}.mp3"
         self.script_write_path = write_dir / f"{input_script.stem}.json"
 
-    def _build_output_script(self) -> None:
+    def _build_output_script(self) -> dict:
         language_code = self.input_script.language_code
         country_code = self.input_script.country_code
         input_lines = self.input_script.lines
         timings = [
-            (segment.start_time_seconds, segment.end_time_seconds)
+            LineTiming(
+                # Timing must be in whole milliseconds.
+                int(segment.start_time_seconds * 1000),
+                int(segment.end_time_seconds * 1000),
+            )
             for segment in self.response.segments
         ]
+        script = {
+            "locale": {
+                "languageCode": language_code,
+                **({"countryCode": country_code} if country_code else {}),
+            },
+            "lines": [
+                {
+                    "startTime": timing.start,
+                    "endTime": timing.end,
+                    "speaker": line.speaker,
+                    "text": line.text,
+                }
+                for line, timing in zip(input_lines, timings)
+            ],
+        }
+        return script
 
     def write_output_script(self) -> None:
         pass
